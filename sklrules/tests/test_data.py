2e4902c7996a5caab5ac4f1ad6583ae9a63c0a9f
f-beck <fbeck@faw.jku.at>
2020-09-09 11:16:41 +0200

    Fix ripper to use just nominal data and replace pipeline

--- sklrules/tests/test_data.py
+++ sklrules/tests/test_data.py
@@ -2,14 +2,8 @@ import numpy as np
 import pandas as pd
 import wittgenstein as lw
 
-from sklearn.model_selection import cross_val_score
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklrules import OneHotEncoder, RuleNetworkClassifier, TypeSelector
-
-pipe = make_pipeline(
-    TypeSelector(np.number, False),
-    OneHotEncoder(sparse=False, handle_unknown='ignore'),
-)
 
 datasets = ['adult', 'airline-passenger-satisfaction', 'alpha-bank', 'bank',
             'breast-cancer', 'credit-g', 'hepatitis', 'kr-vs-kp',
@@ -17,43 +11,54 @@ datasets = ['adult', 'airline-passenger-satisfaction', 'alpha-bank', 'bank',
 RANDOM_STATE = 0
 POS_CLASS_METHOD = 'least-frequent'
 
+ts = TypeSelector(np.number, False)
+ohe = OneHotEncoder(sparse=False, handle_unknown='ignore')
+skf = StratifiedKFold(n_splits=5)
+
 
 def test():
     for idx, dataset in enumerate(datasets):
+        print('\nDataset', dataset)
         X, y, target = __process_dataset(dataset)
-        X = pipe.fit_transform(X).astype(bool)
+        X = ts.fit_transform(X)
+        X = ohe.fit_transform(X).astype(bool)
         fit_params = {
-            'attributes': pipe['onehotencoder'].attributes_,
-            'attribute_lengths': pipe['onehotencoder'].attribute_lengths_,
-            'features': pipe['onehotencoder'].features_,
+            'attributes': ohe.attributes_,
+            'attribute_lengths': ohe.attribute_lengths_,
+            'features': ohe.features_,
             'target': target
         }
-        print(cross_val_score(RuleNetworkClassifier(
-            pos_class_method=POS_CLASS_METHOD), X, y, cv=2,
+        print('RNC(probabilistic):', cross_val_score(RuleNetworkClassifier(
+            pos_class_method=POS_CLASS_METHOD), X, y, cv=skf,
             fit_params=fit_params))
 
 
 def test_ripper():
     ripper = lw.RIPPER(random_state=RANDOM_STATE)
     for idx, dataset in enumerate(datasets):
+        print('\nDataset', dataset)
         X, y, target = __process_dataset(dataset)
         classes, class_counts = np.unique(y, return_counts=True)
         if POS_CLASS_METHOD == 'most-frequent':
             pos_class = classes[class_counts.argmax()]
         else:
             pos_class = classes[class_counts.argmin()]
+        X = ts.fit_transform(X)
         ripper.fit(X, y, pos_class=pos_class)
-        X = pipe.fit_transform(X).astype(bool)
+        fit_params = {'pos_class': pos_class}
+        print('RIPPER:', cross_val_score(ripper, X, y, cv=skf,
+                                         fit_params=fit_params))
+        X = ohe.fit_transform(X).astype(bool)
         fit_params = {
-            'attributes': pipe['onehotencoder'].attributes_,
-            'attribute_lengths': pipe['onehotencoder'].attribute_lengths_,
-            'features': pipe['onehotencoder'].features_,
+            'attributes': ohe.attributes_,
+            'attribute_lengths': ohe.attribute_lengths_,
+            'features': ohe.features_,
             'target': target
         }
-        print(cross_val_score(RuleNetworkClassifier(
+        print('RNC(RIPPER):', cross_val_score(RuleNetworkClassifier(
             init_method='ripper', ripper_model=ripper.ruleset_,
-            pos_class_method=POS_CLASS_METHOD), X, y,
-            cv=2, fit_params=fit_params))
+            pos_class_method=POS_CLASS_METHOD), X, y, cv=skf,
+            fit_params=fit_params))
 
 
 def __process_dataset(dataset):
